7b366b53a7ada2ae688205f5bf2a072d51ec125f
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2024-12-19 20:45:40 +0100

    Update Configuration (#11)
    * New configuration options for source and target directories
    * New include option for files that are not in the src tree
    * Cleanup and consistency updates
    * New debug messages

--- src/main/java/com/github/ijustleyxo/packmake/Config.java
+++ src/main/java/com/github/ijustleyxo/packmake/Config.java
@@ -11,13 +11,18 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.github.ijustleyxo.packmake.Util.gitTag;
+
 public record Config (
-    @NotNull String name,
-    @Nullable String ver,
-    @NotNull List<Integer> formats, // Formats to compile
-    @NotNull List<String> extensions // Allowed file extensions
+        @NotNull String name, // Project name
+        @Nullable String version,// Project version (optional, uses console argument or most recent tag on current git branch instead)
+        @NotNull File src, // Source directory (optional, defaults to src/)
+        @NotNull File target, // Target directory (optional, defaults to target/)
+        @NotNull List<Integer> formats, // Formats to compile
+        @NotNull List<File> include, // Files to include in every package
+        @NotNull List<String> extensions // Allowed file extensions
 ) {
-    public static @NotNull Config load(@NotNull File file) throws ConfigLoadException {
+    public static @NotNull Config load(@NotNull File file, @NotNull String[] args) throws ConfigLoadException {
         Object yml;
 
         try {
@@ -29,15 +34,47 @@ public record Config (
 
         try {
             HashMap<String, Object> fields = (HashMap<String, Object>) yml;
-            if (fields == null) return new Config("NoName", null, new LinkedList<>(), new LinkedList<>());
-            String name = (String) fields.get("name");
+
+            String name = null;
+            String version = null;
+
+            String srcStr = null;
+            String targetStr = null;
+
+            List<Integer> formats = null;
+            List<String> include = null;
+            List<String> extensions = null;
+
+            if (fields != null) {
+                name = (String) fields.get("name");
+                version = (String) fields.get("version");
+
+                srcStr = (String) fields.get("src");
+                targetStr = (String) fields.get("target");
+
+                formats = (List<Integer>) fields.get("formats");
+                include = (List<String>) fields.get("include");
+                extensions = (List<String>) fields.get("extensions");
+            }
+
             if (name == null) name = "NoName";
-            String ver = (String) fields.get("ver");
-            List<Integer> formats = (List<Integer>) fields.get("formats");
+
+            if (args.length > 0) version = args[0];
+            else if (version == null) {
+                String git = gitTag();
+                if (git != null) version = git;
+            }
+
+            if (srcStr == null) srcStr = "./src/";
+            File src = new File(srcStr);
+            if (targetStr == null) targetStr = "./target/";
+            File target = new File(targetStr);
+
             if (formats == null) formats = new LinkedList<>();
-            List<String> extensions = (List<String>) fields.get("extensions");
+            if (include == null) include = new LinkedList<>();
             if (extensions == null) extensions = new LinkedList<>();
-            return new Config(name, ver, formats, extensions);
+
+            return new Config(name, version, src, target, formats, include.stream().map(File::new).toList(), extensions);
         } catch (ClassCastException e) {
             throw new ConfigLoadException(file, "Invalid config format", e);
         }
